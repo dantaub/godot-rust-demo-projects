3df430313e53d3f5f0738c5750ad35a4796713b4
dantaub <33430862+dantaub@users.noreply.github.com>
2025-07-26 15:05:18 -0400

    Simplify repo to single kill-the-creeps demo

--- build-wasm.sh
+++ build-wasm.sh
@@ -4,11 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# Must be in dodge-the-creep's rust directory in order to pick up the .cargo/config
-cd `dirname "$0"`
+# Must be in the project root in order to pick up the .cargo/config
+cd "$(dirname "$0")"
 
-# We build the host gdextension first so that the godot editor doesn't complain.
-cargo +nightly build --package dodge-the-creeps &&
-cargo +nightly build --package dodge-the-creeps \
+# Build the host gdextension first so that the Godot editor doesn't complain.
+cargo +nightly build --package kill-the-creeps &&
+cargo +nightly build --package kill-the-creeps \
   --features godot/experimental-wasm,godot/lazy-function-tables \
   --target wasm32-unknown-emscripten -Zbuild-std $@
